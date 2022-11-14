bc1690aae9fb368b5974c9c48a9b378c1b276735
Erick <erick.santillan@epn.edu.ec>
2022-11-14 10:45:57 -0500

    Primer

---
+++ src/universidades.java
@@ -0,0 +1,12 @@
+public class universidades {
+    int edad;
+    public universidades(){
+        edad = 0;
+    }
+    public int getEdad(){
+    return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+}
